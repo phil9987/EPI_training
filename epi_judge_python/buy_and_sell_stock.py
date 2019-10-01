349529331fe5765d75958bebc4a1e6f472280556
phil9987 <philipjunker@gmail.com>
2019-10-01 09:48:47 +0200

    5.6

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices):
-    # TODO - you fill in here.
-    return 0.0
+    max_profit = 0.0
+    current_min = prices[0]
+    for p in prices:
+        current_min = min(current_min, p)
+        max_profit = max(max_profit, p - current_min)
+    return max_profit
 
 
 if __name__ == '__main__':
