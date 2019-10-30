39de266047b96b99f02386669b0662d39bf9967c
phil9987 <philipjunker@gmail.com>
2019-10-30 15:15:31 +0100

    reverse singly linked list partially and complete

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,9 +1,35 @@
 from test_framework import generic_test
+from list_node import ListNode
 
 
 def reverse_sublist(L, start, finish):
-    # TODO - you fill in here.
-    return None
+    if start == finish:
+        return L
+    dummy_head = ListNode(0, L)
+    pre_head = dummy_head
+    for i in range(start-1):
+        pre_head = pre_head.next
+    tail = pre_head
+    for _ in range(finish - start + 1):
+        tail = tail.next
+    post_tail = tail.next
+    tail.next = None
+    new_head, new_tail = reverse(pre_head.next)
+    pre_head.next = new_head
+    new_tail.next = post_tail
+    return dummy_head.next
+
+
+def reverse(L: ListNode) -> ListNode:
+    last_node = L
+    prev = None
+    curr = L
+    while curr:
+        next_ = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_
+    return prev, last_node
 
 
 if __name__ == '__main__':
