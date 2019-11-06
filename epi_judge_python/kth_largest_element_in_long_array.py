9cd011a082f72abe0e30bab229166e9bb8683f51
phil9987 <philipjunker@gmail.com>
2019-11-06 11:02:38 +0100

    kth largest el

--- epi_judge_python/kth_largest_element_in_long_array.py
+++ epi_judge_python/kth_largest_element_in_long_array.py
@@ -1,11 +1,19 @@
 from typing import Iterator
 
 from test_framework import generic_test
-
+import heapq
 
 def find_kth_largest_unknown_length(stream: Iterator[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    min_heap = []
+    for el in stream:
+        if len(min_heap) < k:
+            heapq.heappush(min_heap, el)
+        elif el > min_heap[0]:
+            heapq.heappushpop(min_heap, el)
+    if len(min_heap) == k:
+        return min_heap[0]
+    else:
+        raise Exception("k larger than stream length, no kth largest element exists")
 
 
 # Pythonic solution that uses library method but costs O(nlogk) time.
@@ -18,6 +26,7 @@ def find_kth_largest_unknown_length_wrapper(stream, k):
 
 
 if __name__ == '__main__':
+    find_kth_largest_unknown_length_wrapper([1,2,3,4,5], 3)
     exit(
         generic_test.generic_test_main(
             'kth_largest_element_in_long_array.py',
